b0908712e7fa8b09d5118fcdc24712ee3b73911b
Demo User <demouser@MacBook-Pro.local>
2018-07-17 12:35:24 -0700

    JavaScript-changes to javascript.js

--- Teststuffs/javaScriptTest/javascript.js
+++ Teststuffs/javaScriptTest/javascript.js
@@ -7,11 +7,15 @@ console.log(`Hello ${name}!`)
 const age = 16
 console.log(`You are ${age} years Old!`)
 
-if(age >=16)
+if(age >= 18)
 {
-  console.log("You are old enough to drive a car")
+  console.log("You are old enough to drive a car and vote.")
+}
+else if(age < 15)
+{
+  console.log('You are not old enough to drive or vote')
 }
 else
 {
-  console.log('You are not old enough to drive a car')
+  console.log('You are old enough to drive a car but you are not old enough to vote.')
 }
