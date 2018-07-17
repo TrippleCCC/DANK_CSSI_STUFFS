2f3a1f4709b8da439d833acb4cf721047a95c44b
Demo User <demouser@MacBook-Pro.local>
2018-07-17 11:31:25 -0700

    JavaScript Introduction

---
+++ Teststuffs/javaScriptTest/javascript.js
@@ -0,0 +1,17 @@
+console.log('Hello World!');
+
+
+const name = 'Charlie'
+console.log(`Hello ${name}!`)
+
+const age = 16
+console.log(`You are ${age} years Old!`)
+
+if(age >=16)
+{
+  console.log("You are old enough to drive a car")
+}
+else
+{
+  console.log('You are not old enough to drive a car')
+}
