2bcbebbeac601b2250fe8e45b60e0c151e4c8059
Demo User <demouser@MacBook-Pro.local>
2018-07-19 13:27:10 -0500

    started python

---
+++ Teststuffs/python_tests/my_first_python.py
@@ -0,0 +1,61 @@
+"""This is my file
+
+
+This is the first python file I have written, sort of (not really).
+"""
+
+
+print('Hello World!')
+
+if 1 == 2:
+    print("wuuuuut")
+else:
+    print('ha!')
+
+x = 1
+while x <= 5:
+    print(x)
+    print("HA!")
+    x += 1
+    #this is a comment
+    # Other
+
+
+for thing in range(5):
+    print(thing)
+
+for thing in "hello":
+    print(thing)
+
+my_list = [1,2,3,4,5]
+for bob in my_list:
+    print(bob)
+
+for val in range(15, 100):
+    print('{val} is divisible by 2: {donno}'.format(
+        val=val, donno=val%2 == 0
+    ))
+
+
+def my_function():
+    print('hello lunchtime')
+    return 'hello lunchtime'
+
+my_function()
+
+def fizzBuzz(num):
+    if(num % 3 == 0 and num % 5 == 0):
+        print("FizzBuzz")
+    elif(num % 3 == 0):
+        print("Fizz")
+    elif(num % 5 == 0):
+        print("Buzz")
+
+def betterFizzBuzz(num):
+    #print("{f}{b}".format(f = "Fizz" if num % 3 == 0 else "", b = "Buzz" if num % 5 == 0 else ""))
+    return "{f}{b}".format(f = "Fizz" if num % 3 == 0 else "", b = "Buzz" if num % 5 == 0 else "")
+
+
+def rangedFizzBuzz(bot, top):
+    for i in range(bot, top+1):
+        print(str(i) + " is " + str(betterFizzBuzz(i)))
---
+++ python/labs/make-a-short-story/mystory.py
@@ -0,0 +1,18 @@
+"""
+My Story Lab
+
+"""
+
+lineOne = "The once was a {gen} named {n}."
+lineTwo = "{n} liked {f}."
+lineThree = "{n} once ate a lot of {f}s."
+lineFour = "Afterwards, {n} felt really sick."
+lineFive = "the end".upper()
+
+
+gen = input("Male or Female?: ")
+name = input("Give me a nice name: ")
+food = input("What is your favorite food?: ")
+
+
+print(lineOne.format(ge))
